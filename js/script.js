f701c45fe46c3f506f528e273b6cff8253a956dd
Marilia Macedo <maritamisguemacedo@gmail.com>
2020-05-28 16:18:36 +0100

    Create snake movement

--- js/script.js
+++ js/script.js
@@ -7,20 +7,44 @@ snake[0] = {
     y: 8 * box
 };
 
+let direction = 'right';
+
 function createBG() {
     context.fillStyle = "#191970";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function createSnake() {
-    for(i=0;i<snake.length;i++){
+    for (i = 0; i < snake.length; i++) {
         context.fillStyle = "#FFFF00";
-        context.fillRect(snake[i].x,snake[i].y,box,box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-createBG();
-createSnake();
+function startGame() {
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == 'right') snakeX+=box;
+    if(direction == 'left') snakeX-=box;
+    if(direction == 'up') snakeY+=box;
+    if(direction == 'down') snakeY-=box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(startGame,100);
+
 
 
 
