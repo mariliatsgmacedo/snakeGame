865de197619b3e3800243a159952d7c968eccdd1
Marilia Macedo <maritamisguemacedo@gmail.com>
2020-05-28 17:14:32 +0100

    bug fix

--- js/script.js
+++ js/script.js
@@ -27,23 +27,22 @@ function createSnake() {
 
 function createFood() {
     context.fillStyle = "red";
-    context.fillRect(food.x, food.y,box,box);
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
 function update(event) {
-    if (event.keyCode == 39 && direction != 'left') direction = 'right';
-    if (event.keyCode == 40 && direction != 'dow') direction = 'up';
     if (event.keyCode == 37 && direction != 'right') direction = 'left';
-    if (event.keyCode == 38 && direction != 'up') direction = 'down';
-
+    if (event.keyCode == 38 && direction != 'down') direction = 'up';
+    if (event.keyCode == 39 && direction != 'left') direction = 'right';
+    if (event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
 function startGame() {
     if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
     if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
-    if (snake[0].y > 15 * box && direction == 'down') snake[0].x = 0;
-    if (snake[0].y < 0 && direction == 'up') snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
+    if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
     createBG();
     createSnake();
@@ -54,10 +53,16 @@ function startGame() {
 
     if (direction == 'right') snakeX += box;
     if (direction == 'left') snakeX -= box;
-    if (direction == 'up') snakeY += box;
-    if (direction == 'down') snakeY -= box;
+    if (direction == 'up') snakeY -= box;
+    if (direction == 'down') snakeY += box;
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box,
+            food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
+
 
     let newHead = {
         x: snakeX,
